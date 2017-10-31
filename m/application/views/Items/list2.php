bc6b66c2d6f8b5abdb34bf980b8c8be70f37084e
dusdww <dusdww@qq.com>
2017-10-31 23:13:07 +0800

    123

--- m/application/views/Items/list2.php
+++ m/application/views/Items/list2.php
@@ -6,68 +6,58 @@
     <meta name="renderer" content="webkit">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <link rel="shortcut icon" href="//www.ebay.cn/favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" href="<?php echo APP_URL ?>/public/css/items.css" type="text/css"/>
+    <link rel="stylesheet" href="<?php echo APP_URL ?>/public/css/mobile/list2.css" type="text/css"/>
     <script src="<?php echo APP_URL ?>/public/js/jquery-1.8.2.min.js" type="text/javascript"></script>
 </head>
 
 <body>
-<div id="banner" class="flexslider">
-    <ul class="slides">
-        <?php foreach ($banners as $banner): ?>
-            <li>
-                <a href="<?php echo $banner['href'] ?>" target="_blank" title="<?php echo $banner['title'] ?>">
-                    <img src="<?php echo $banner['image'] ?>" title="<?php echo $banner['title'] ?>" alt="<?php echo $banner['title'] ?>" />
-                    <div class="slider-title"><?php echo $banner['title'] ?></div>
-                </a>
-            </li>
-        <?php endforeach ?>
-    </ul>
-</div><!--flexslider end-->
-
-<!-- list -->
-<div id="list" class="wpn mtw">
-    <div class="list-title">
-        <span class="title">EBAY大卖</span>
-        <!--<span class="more">更多 >></span>-->
+<div class="g-doc">
+    <div class="g-wrap m-wrap1">
+        <div id="banner" class="flexslider">
+            <ul class="slides">
+                <?php foreach ($banners as $banner): ?>
+                    <li>
+                        <a href="<?php echo $banner['href'] ?>" target="_blank" title="<?php echo $banner['title'] ?>">
+                            <img src="<?php echo $banner['image'] ?>" title="<?php echo $banner['title'] ?>" alt="<?php echo $banner['title'] ?>" />
+                            <div class="slider-title"><?php echo $banner['title'] ?></div>
+                        </a>
+                    </li>
+                <?php endforeach ?>
+            </ul>
+        </div>
     </div>
-    <ul class="list cl">
-        <?php foreach ($products as $product): ?>
-            <li class="list-item">
-                <!-- 内容列表项图片 -->
-                <div class="image-banner">
-                    <a href="<?php echo $product['href'] ?>" title="<?php echo $product['title'] ?>" target="_blank">
-                        <img class="image" src="<?php echo $product['image'] ?>" alt="<?php echo $product['title'] ?>"/>
-                    </a>
-                </div>
-                <!-- 内容列表项信息 -->
-                <div class="info">
-                    <a href="<?php echo $product['href'] ?>">
-                        <h4 class="title"><?php echo $product['title'] ?></h4>
-                    </a>
-                    <a href="<?php echo $product['href'] ?>">
-                        <p class="describe"><?php echo $product['description'] ?></p>
-                    </a>
-                    <a href="<?php echo $product['href'] ?>">
-                        <span class="price">￥ <?php echo $product['sale_price'] ?></span>
-                        <span class="old-price">￥ <?php echo $product['real_price'] ?></span>
-                    </a>
-                </div>
-            </li>
-        <?php endforeach ?>
-    </ul>
-
-    <script type="text/javascript" src="<?php echo APP_URL ?>/public/js/slider.js"></script>
-    <script type="text/javascript">
-        $(function(){
-            $('#banner').flexslider({
-                animation: "fade", //转换方式 fade淡入淡出 slide滚动
-                direction:"horizontal", //滚动方向 horizontal左右 vertical上下
-                slideshowSpeed: 3000, //停留时间
-                directionNav: true, //是否显示左右控制按钮 true&false
-                controlNav: true, //是否显示下方控制按钮 true&false
-                mousewheel: false, //是否允许鼠标控制滚动 true&false
-            });
+    <div class="g-wrap m-wrap5">
+        <div class="m-goods-gather">
+            <ul class="m-goods-list">
+                <?php foreach ($products as $product): ?>
+                    <li class="m-goods-item">
+                        <a href="<?php echo $product['href'] ?>" class="m-goods-con PSC_J_normal_statistics_Goods">
+                            <div class="u-pic">
+                                <img src="<?php echo $product['image'] ?>" class="pic">
+                            </div>
+                            <div class="u-desc"><?php echo $product['description'] ?></div>
+                            <div class="u-detail">
+                                <p class="name f-tof"><?php echo $product['title'] ?></p>
+                                <p class="price-box"><span class="price"><?php echo $product['sale_price'] ?></span></p>
+                            </div>
+                        </a>
+                    </li>
+                <?php endforeach ?>
+            </ul>
+        </div>
+    </div>
+</div>
+<script type="text/javascript" src="<?php echo APP_URL ?>/public/js/slider.js"></script>
+<script type="text/javascript">
+    $(function(){
+        $('#banner').flexslider({
+            animation: "fade", //转换方式 fade淡入淡出 slide滚动
+            direction:"horizontal", //滚动方向 horizontal左右 vertical上下
+            slideshowSpeed: 3000, //停留时间
+            directionNav: true, //是否显示左右控制按钮 true&false
+            controlNav: true, //是否显示下方控制按钮 true&false
+            mousewheel: false, //是否允许鼠标控制滚动 true&false
         });
-
-    </script>
+    });
+</script>
 </body>
