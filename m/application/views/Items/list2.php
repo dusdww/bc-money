f74bc77a574e6058d7c8d8f8150427503cdecba2
dusdww <dusdww@qq.com>
2017-11-01 19:17:09 +0800

    33

--- m/application/views/Items/list2.php
+++ m/application/views/Items/list2.php
@@ -56,9 +56,9 @@
             animation: "fade", //转换方式 fade淡入淡出 slide滚动
             direction:"horizontal", //滚动方向 horizontal左右 vertical上下
             slideshowSpeed: 3000, //停留时间
-            directionNav: true, //是否显示左右控制按钮 true&false
+            directionNav: false, //是否显示左右控制按钮 true&false
             controlNav: true, //是否显示下方控制按钮 true&false
-            mousewheel: false, //是否允许鼠标控制滚动 true&false
+            mousewheel: true, //是否允许鼠标控制滚动 true&false
             pauseOnAction: false,
         });
     });
