9de1c61ea41d859d484fcd2e32b5224db0016c68
dusdww <dusdww@qq.com>
2017-11-01 11:49:41 +0800

    123

--- m/application/views/Items/list1.php
+++ m/application/views/Items/list1.php
@@ -1,7 +1,13 @@
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>demo</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>ebay活动大促</title>
+    <meta name="keywords" content="海淘在eBay | 海量库存 奥莱价格 安心海淘">
+    <meta name="description" content="海淘在eBay | 海量库存 奥莱价格 安心海淘">
+    <meta name="renderer" content="webkit">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <link rel="shortcut icon" href="//www.ebay.cn/favicon.ico" type="image/x-icon">
     <link href="<?php echo APP_URL ?>/public/css/mobile/index1.css" rel="stylesheet" type="text/css" />
+    <script src="<?php echo APP_URL ?>/public/js/jquery-1.8.2.min.js" type="text/javascript"></script>
 </head>
 <body>
 <div class="index">
@@ -13,6 +19,18 @@
         <div class="title">
             <P>30年专业音响</P>
         </div>
+    </div>
+    <div id="banner" class="flexslider">
+        <ul class="slides">
+            <?php foreach ($banners as $banner): ?>
+                <li>
+                    <a href="<?php echo $banner['href'] ?>" target="_blank" title="<?php echo $banner['title'] ?>">
+                        <img src="<?php echo $banner['image'] ?>" title="<?php echo $banner['title'] ?>" alt="<?php echo $banner['title'] ?>" />
+                        <div class="slider-title"><?php echo $banner['title'] ?></div>
+                    </a>
+                </li>
+            <?php endforeach ?>
+        </ul>
     </div>
     <div class="content">
         <div class="headline">
@@ -40,4 +58,17 @@
         </div>
     </div>
 </div>
+<script type="text/javascript" src="<?php echo APP_URL ?>/public/js/slider.js"></script>
+<script type="text/javascript">
+    $(function(){
+        $('#banner').flexslider({
+            animation: "fade", //转换方式 fade淡入淡出 slide滚动
+            direction:"horizontal", //滚动方向 horizontal左右 vertical上下
+            slideshowSpeed: 3000, //停留时间
+            directionNav: true, //是否显示左右控制按钮 true&false
+            controlNav: true, //是否显示下方控制按钮 true&false
+            mousewheel: false, //是否允许鼠标控制滚动 true&false
+        });
+    });
+</script>
 </body>
