146b17e50ce7a9ddb1d57b9af6511072893b676f
dusdww <dusdww@sohu.com>
2018-03-05 19:50:10 +0800

    0305~~

--- m/application/controllers/ItemsController.class.php
+++ m/application/controllers/ItemsController.class.php
@@ -4,12 +4,13 @@ class ItemsController extends Controller {
     // 首页方法
     public function index() {
         $banners = (new LinksModel)->selectAllByColumnValue('type',0);
-        $products = (new ProductsModel)->selectAllByColumnValue('type',0);
+        $products = (new ProductsModel)->selectAllByColumnValue('enabled',0);
 
         $this->assign('banners', $banners);
         $this->assign('products', $products);
     }
-    function view($id = null) {
+
+    public function view($id = null) {
         $channel =  (new ChannelsModel)->select($id);
         $banners = (new LinksModel)->selectAllByColumnValue('type',0);
         $products = (new ProductsModel)->selectAllByColumnValue('enabled',1);
