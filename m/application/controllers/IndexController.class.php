9724e26598bbf415086695518ef25aed70063c25
dusdww <dusdww@sohu.com>
2018-03-05 20:00:26 +0800

    0305~~

--- m/application/controllers/IndexController.class.php
+++ m/application/controllers/IndexController.class.php
@@ -4,7 +4,7 @@ class IndexController extends Controller {
     // 首页方法
     public function index() {
         $banners = (new LinksModel)->selectAllByColumnValue('type',0);
-        $products = (new ProductsModel)->selectAllByColumnValue('enabled',0);
+        $products = (new ProductsModel)->selectAll();
 
         $this->assign('banners', $banners);
         $this->assign('products', $products);
